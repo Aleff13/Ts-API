fce33cf3c8e4a0bb4d004eec6620072bbd8de57c
aleff menna resler <83561751+Aleff13@users.noreply.github.com>
2022-08-29 14:06:12 -0300

    fix: doc update

--- src/routes/itens-routes.ts
+++ src/routes/itens-routes.ts
@@ -6,7 +6,7 @@ const itensRouter = express.Router();
 
 itensRouter.get('/doc', (req, res) => {
     res.send(
-"<style> table.GeneratedTable { width: 100%; background-color: #ffffff; border-collapse: collapse; border-width: 2px; border-color: #cfcfcf; border-style: solid; color: #000000; } table.GeneratedTable td, table.GeneratedTable th { border-width: 2px; border-color: #cfcfcf; border-style: solid; padding: 3px; } table.GeneratedTable thead { background-color: #d08c3e; } </style> <table class='GeneratedTable'> <thead> <tr> <th>Type</th> <th>Route</th> <th>Body</th> </tr> </thead> <tbody> <tr> <td>GET</td> <td>/api/itens</td> <td>null</td> </tr> <tr> <td>GET</td> <td>/api/item/{id}</td> <td>null</td> </tr> <tr> <td>POST</td> <td>/api/itens</td> <td> <code> { 'nome': 'Lamborghini Aventator', <br> 'descricao': 'It has all the standard features of the Aventador S and SVJ. It produces a maximum output of 780 PS (574 kW; 769 hp) and the same 720 N⋅m (531 lb⋅ft) of torque as the Aventador SVJ. Lamborghini claims that it can accelerate from 0–100 km/h (0–62 mph) in 2.8 seconds. The top speed is claimed to be 356 km/h (221 mph).', <br> 'preco' : 1000 <br>}, </code></td> </tr> <tr> <td>PUT</td> <td>/api/itens</td> <td> <code> { 'id': 3, <br> 'nome': 'Lamborghini Aventator', <br> 'descricao': 'It has all the standard features of the Aventador S and SVJ. It produces a maximum output of 780 PS (574 kW; 769 hp) and the same 720 N⋅m (531 lb⋅ft) of torque as the Aventador SVJ. Lamborghini claims that it can accelerate from 0–100 km/h (0–62 mph) in 2.8 seconds. The top speed is claimed to be 356 km/h (221 mph).', <br> 'preco' : 1000 <br>}, </code></td> </tr><td>DELETE</td> <td>/api/item/{id}</td> <td>null</td></tbody> </table>"
+'<style> table.GeneratedTable { width: 100%; background-color: #ffffff; border-collapse: collapse; border-width: 2px; border-color: #cfcfcf; border-style: solid; color: #000000; } table.GeneratedTable td, table.GeneratedTable th { border-width: 2px; border-color: #cfcfcf; border-style: solid; padding: 3px; } table.GeneratedTable thead { background-color: #d08c3e; } </style> <table class="GeneratedTable"> <thead> <tr> <th>Type</th> <th>Route</th> <th>Body</th> </tr> </thead> <tbody> <tr> <td>GET</td> <td>/api/itens</td> <td>null</td> </tr> <tr> <td>GET</td> <td>/api/item/{id}</td> <td>null</td> </tr> <tr> <td>POST</td> <td>/api/itens</td> <td> <code> { "nome": "Lamborghini Aventator", <br> "descricao": "It has all the standard features of the Aventador S and SVJ. It produces a maximum output of 780 PS (574 kW; 769 hp) and the same 720 N⋅m (531 lb⋅ft) of torque as the Aventador SVJ. Lamborghini claims that it can accelerate from 0–100 km/h (0–62 mph) in 2.8 seconds. The top speed is claimed to be 356 km/h (221 mph).", <br> "preco" : 1000 <br>}, </code></td> </tr> <tr> <td>PUT</td> <td>/api/itens</td> <td> <code> { "id": 3, <br> "nome": "Lamborghini Aventator", <br> "descricao": "It has all the standard features of the Aventador S and SVJ. It produces a maximum output of 780 PS (574 kW; 769 hp) and the same 720 N⋅m (531 lb⋅ft) of torque as the Aventador SVJ. Lamborghini claims that it can accelerate from 0–100 km/h (0–62 mph) in 2.8 seconds. The top speed is claimed to be 356 km/h (221 mph).", <br> "preco" : 1000 <br>}, </code></td> </tr><td>DELETE</td> <td>/api/item/{id}</td> <td>null</td></tbody> </table>'
         )
 })
 
@@ -16,7 +16,14 @@ itensRouter.get('/itens', (req, res) => {
 
 itensRouter.get('/item/:id', (req, res) => {
     const id: number = +req.params.id
-    itensRepository.findById(id, (item) => res.status(200).json(item))
+    itensRepository.findById(id, (item) => {
+    
+    // if(!item){
+    //     res.status(404).json()
+    //     return;
+    // }
+    
+    res.status(200).json(item)})
 })
 
 itensRouter.delete('/item/:id', (req, res) => {
